048c0f217d184952b1d737038df9207ebdbb1c2f
tom-grm88 <tom.germain88@gmail.com>
2025-05-14 09:58:57 +0200

    Probleme Multiplication spectre

--- Multiplication simple des spectres.py
+++ Multiplication simple des spectres.py
@@ -3,20 +3,22 @@ import librosa
 import matplotlib.pyplot as plt
 from scipy.signal import find_peaks
 
+# PROBLEME
 
 # --- Paramètres ---
-A4_FREQ = 440.0
+freq_A1 = 27.5
 notes = ['C', 'C#', 'D', 'D#', 'E', 'F',
         'F#', 'G', 'G#', 'A', 'A#', 'B']
 
-# --- Conversion fréquence → note ---
+
 def freq_to_note(freq):
     A_index = 9  # Position de A dans la liste des notes
     if freq <= 0:
         return None
-    n_demi_tons = round(12 * np.log2(freq / 440))
+    n_demi_tons = round(12 * np.log2(freq / freq_A1))
     note_index = (A_index + n_demi_tons) % 12
-    return notes[note_index]
+    octave = (A_index + n_demi_tons) // 12
+    return notes[note_index] + str(octave)
 
 
 # --- Détection des pics ---
@@ -39,8 +41,8 @@ dico = {}
 spectre_combiné = None
 for f in fichiers :
     dico[f]=[]
-    audio, sr = librosa.load(f)
-    audio = audio[1000:1000+4*sr]   # trim
+    audio, sr = librosa.load(f, sr = 44100)
+    audio = audio[2*sr:4*sr]   # trim
     freqs = np.fft.rfftfreq(len(audio), 1 / sr)
     spectre = np.abs(np.fft.rfft(audio))
     spectre /= max(spectre)
@@ -58,25 +60,21 @@ freqs= dico["Audio_YT/C4 YT.mp3"][1]
 
 i_peaks, _ = find_peaks(spectre_combiné, height=np.max(spectre_combiné) * 0.1)
 freq_peaks = freqs[i_peaks]
-# score = énergie des pics / énergie totale
-score = np.sum(spectre_combiné[i_peaks]) / np.sum(spectre_combiné)
-score = min(score * 100, 100)
 
-print("Score :", score)
 
 # --- Affichage ---
 plt.figure(figsize=(10, 4))
-'''
+
 for f in freq_peaks:
     plt.axvline(x=f, color='r', linestyle='--', alpha=0.6)
-'''
+
 plt.plot(freqs, spectre_combiné,label='res')
 
 plt.plot(dico["Audio_YT/C4 YT.mp3"][1], dico["Audio_YT/C4 YT.mp3"][2],label='C4')
 plt.plot(dico["Audio_YT/E4 YT.mp3"][1], dico["Audio_YT/E4 YT.mp3"][2],label='E4')
 plt.plot(dico["Audio_YT/G4 YT.mp3"][1], dico["Audio_YT/G4 YT.mp3"][2],label='G4')
 
-plt.xlim(0, 2000)
+plt.xlim(0, 4000)
 plt.grid(True)
 plt.legend()
 plt.show()
